fd3c0f831add7bebb72eb2754cc5adfd0f180d32
홍은표 <44293278+potato179@users.noreply.github.com>
2022-08-15 21:29:29 +0900

    디데이 구하기

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 window.onload = function(){
     setInterval(setClock, 1000);
+    setInterval(getMoney, 1000);
 }
 
 function setClock(){
     var dateInfo = new Date(); 
     var year = dateInfo.getFullYear();
-    var month = modifyNumber(dateInfo.getMonth()+1);
-    var date = modifyNumber(dateInfo.getDate());
+    var month = dateInfo.getMonth()+1;
+    var date = dateInfo.getDate();
     
     var dateDisplay = `오늘날짜: ${year}년 ${month}월 ${date}일`;
     $("#date").html(dateDisplay);
+    getMoney(`${year}-${month}-${date}`);
 }
 
-function modifyNumber(time){
-    if(parseInt(time) < 10){
-        return "0" + time;
-    }
-    else{
-        return time;
-    }
+function getMoney(todayDate){
+    let tot;
+    const date1 = new Date("2022-07-22");
+    const date2 = new Date(todayDate);
+    
+    const diffDate = date1.getTime() - date2.getTime();
+    console.log(diffDate / (1000 * 60 * 60 * 24));
 }
